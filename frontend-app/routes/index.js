3566de23cdfe170a76a8c64a9dac88c58477f590
smartx-usman <usman@smartx.kr>
2020-12-16 05:23:34 +0100

    Get and display cluster info from backend app. Get and display pod info from backend app.

--- frontend-app/routes/index.js
+++ frontend-app/routes/index.js
@@ -2,11 +2,45 @@
 
 const express = require('express');
 const passport = require('passport');
+const fetch = require('node-fetch');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const { user } = req;
-    res.render('home', { user });
+    var Pod_List='';
+    var Cluster_List /*= [
+        { cluster: 'KR-GIST', running: 'true' },
+        { cluster: 'MY-UM', running: 'false' },
+        { cluster: 'TH-CHULA', running: 'true' }
+    ]*/;
+    var backend_api_base_url = 'https://oftein-backend-deployment-pod-f5oec474zq-an.a.run.app/clusters';
+    var gist_cluster_pods_url = backend_api_base_url + '/k3d_c1/pods'
+    var um_cluster_pods_url = backend_api_base_url + '/k3d_c2/pods'
+    var chula_cluster_pods_url = backend_api_base_url + '/um/pods'
+
+    fetch(backend_api_base_url)
+        .then(response => response.json())
+        .then(cluster_json => {
+            console.log(cluster_json);
+            //res.render('home', { user: user , Cluster_List: Cluster_List, Pod_List: json });
+            fetch(gist_cluster_pods_url)
+                .then(response => response.json())
+                .then(pod_json => {
+                    console.log(pod_json);
+                    res.render('home', {user: user, Cluster_List: cluster_json, Pod_List: pod_json});
+                })
+        })
+
+    /*fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log("First user in the array:");
+        console.log(json[0]);
+        Pod_List=json;
+    });*/
+    /*client.get('/list', function (err, res, body) {
+        return console.log(body);
+    });*/
 });
 
 router.get('/login/facebook', passport.authenticate('facebook'));
